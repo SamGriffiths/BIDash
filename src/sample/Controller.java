89b680d3405a9b61b05e99f604264a46b06d8b44
bowfinger <bowfinger2k1@hotmail.com>
2016-02-09 02:45:12 +0000

    test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -112,6 +112,7 @@ public class Controller implements Initializable {
         }
     }
 
+
     public static <T> Predicate<T> distinctByKey(Function<? super T,Object> keyExtractor) {
         Map<Object,Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
